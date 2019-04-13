17d4a6b73e7cb55ca80a4d8f8b6f2af7658d0d69
Tabita <taby_marcu@yahoo.com>
2019-04-13 19:45:26 +0300

    Update hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -76,6 +76,7 @@ export class HeroService {
       return of([]);
     }
 
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
       tap(_ => this.log(`found heroes matching "${term}"`)),
       catchError(this.handleError<Hero[]>('search Heroes', []))
     );
